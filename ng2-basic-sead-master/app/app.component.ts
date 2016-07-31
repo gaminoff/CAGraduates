d737bb537c787fd0850e58120f1c9d443e2e3490
Gilad Aminoff <giladaminoff@gmail.com>
2016-07-31 15:09:19 +0300

    routing ang navbar

--- ng2-basic-sead-master/app/app.component.ts
+++ ng2-basic-sead-master/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationStart } from '@angular/router';
+
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 
 
@@ -19,4 +20,19 @@ import * as io from 'socket.io-client';
   providers: [GradService, ChatRoomService, ToastsManager, {provide: 'io', useValue: io}]
 
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+
+  private inAdminPage = false ;
+
+
+  constructor(private router: Router) {
+    router.events.filter(event => event instanceof NavigationStart)
+        .subscribe( event => {
+            console.log('event: ', event.url);
+            if ((event.url).includes('/admin')) this.inAdminPage = true ;
+            else if (event.url === '/') this.router.navigate(['/CAGrads']);
+            else this.inAdminPage = false ;
+        })
+  }
+
+}
