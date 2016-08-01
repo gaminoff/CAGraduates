1cd3387a5a26d247570d1f133ce4f9701e492225
Igor I <mr.igfe@gmail.com>
2016-08-01 15:16:38 +0300

    jhyuhv

--- ng2-basic-sead-master/app/admin/grad-filter.component.ts
+++ ng2-basic-sead-master/app/admin/grad-filter.component.ts
@@ -4,9 +4,10 @@ import { Component, OnInit, EventEmitter } from '@angular/core';
   moduleId: module.id,
   selector: 'grad-filter',
   outputs: ['filterChange'],
-  styles: [`section {background-color: #DDD; margin: 2em 0; padding:0.4em 1em 1em; border-radius:0.4em} `],
+   styleUrls: [`../../public/css/styles.css`],
+  // styles: [`section {background-color: #DDD; margin: 2em 0; padding:0.4em 1em 1em; border-radius:0.4em} `],
   template: `
-      <section>
+      <section class="admin filter">
         <h3>Filter</h3>
         By Name: <input type="text" [(ngModel)]="filter.byName" (input)="filterChanged()" />
         By Course: <input type="text" [(ngModel)]="filter.byCourse" (input)="filterChanged()">
