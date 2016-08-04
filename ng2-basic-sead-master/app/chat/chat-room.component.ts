2c2132e443f3a39824da5f6436dcc723f473fbe5
Igor I <mr.igfe@gmail.com>
2016-08-04 17:43:17 +0300

    chat

--- ng2-basic-sead-master/app/chat/chat-room.component.ts
+++ ng2-basic-sead-master/app/chat/chat-room.component.ts
@@ -14,13 +14,13 @@ import {ChatRoomService} from './chat-room.service'
          
                   <div class="total">
                     <button (click)="nick=undefind ; j.focus()">Disconnect</button>
-                    <h2>{{(nick) ? "Well come "+ nick  : "nickName"}}</h2>      
+                    <h2>{{(nick) ? "Well come "+ nick : "nickName"}}</h2>      
                     <input #j (keyup.enter)="nick=j.value; j.value=''" class="chatInput chatNick" autofocus>
                     
                     <div *ngIf="(nick!==undefind && nick!=='')">
                       <h2>{{(chatRoom.connected$ | async) ? "Connected!" : "Disconnected..."}}</h2>
                       
-                      <textarea #i  (keyup.enter)="chatRoom.send$.next(nick +':' +i.value); mess=i.value;i.value = ''" class="chatInput chatMessage" placeholder=" Type your message">
+                      <textarea #i  (keyup.enter)="chatRoom.send$.next(nick +':' +i.value); i.value = ''" class="chatInput chatMessage" placeholder=" Type your message">
                       </textarea>
                     </div>
                     <div  *ngFor="let message of chatRoom.messages$ | async" class="message"  >
