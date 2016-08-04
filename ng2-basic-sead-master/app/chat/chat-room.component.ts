178254d39823314428031525388d3f67b75947f0
Igor I <mr.igfe@gmail.com>
2016-08-04 12:21:27 +0300

    chat

--- ng2-basic-sead-master/app/chat/chat-room.component.ts
+++ ng2-basic-sead-master/app/chat/chat-room.component.ts
@@ -6,21 +6,26 @@ import {ChatRoomService} from './chat-room.service'
     *{font-family: Monaco, Consolas;}
     `],
     template: `
-    <div class="chat">
-      <div class="total">
-        <h2>{{"nickName"}}</h2>      
-        <input #j (keyup.enter)="nick=j.value; j.value=''" class="chatInput chatNick">
-        <div *ngIf="nick!==undefind">
-          <h2>{{(chatRoom.connected$ | async) ? "Connected!" : "Disconnected..."}}</h2>
-          
-          <textarea #i  (keyup.enter)="chatRoom.send$.next(nick +':' +i.value); i.value = ''" class="chatInput chatMessage" placeholder=" Typed year message">
-          </textarea>
-        </div>
-        <div  *ngFor="let message of chatRoom.messages$ | async" class="message"  >
-        {{message}}
-        </div>
+    <div>
+          <div><img src="img/coding-logo.png"></div>
+          <img src="img/3.jpg">
+
+                <div class="chat">
+                  <div class="total">
+                    <h2>{{"nickName"}}</h2>      
+                    <input #j (keyup.enter)="nick=j.value; j.value=''" class="chatInput chatNick">
+                    <div *ngIf="nick!==undefind">
+                      <h2>{{(chatRoom.connected$ | async) ? "Connected!" : "Disconnected..."}}</h2>
+                      
+                      <textarea #i  (keyup.enter)="chatRoom.send$.next(nick +':' +i.value); i.value = ''" class="chatInput chatMessage" placeholder=" Type your message">
+                      </textarea>
+                    </div>
+                    <div  *ngFor="let message of chatRoom.messages$ | async" class="message"  >
+                    {{message}}
+                    </div>
+                  </div>
+                </div>
       </div>
-    </div>
     `
 })
 export class ChatRoomComponent {
